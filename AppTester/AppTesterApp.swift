84cf8892023f9f6dc4393fd68b7e31ebe374f54f
Andrew <andrwe@wielding.net>
2024-09-08 17:41:55 -0400

    Initial Commit

---
+++ AppTester/AppTesterApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppTesterApp.swift
+//  AppTester
+//
+//  Created by Andrew Kunkel on 9/8/24.
+//
+
+import SwiftUI
+
+@main
+struct AppTesterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
